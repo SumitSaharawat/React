b3a04b6eacebfcdbdbbf38aab63cce97dc80fa59
SumitSaharawat <sumitsehrawat726@icloud.com>
2026-04-05 18:33:05 -0500

    Move to mainfile

--- QuizApp/src/App.jsx
+++ QuizApp/src/App.jsx
@@ -1,14 +1,40 @@
 import { useState } from 'react'
 import './App.css'
 import Question from './components/Question'
+import { QuestionContext } from './components/Question'
+import { values } from './components/Values'
 
 
 function App() {
 
+    const questionBank = values[0].questions;
+    const [answer, setAnswer] = useState({});
+    const [qIndex, setQindex] = useState(0);
+
+    const handleAnswers = (index, value) =>{
+        setAnswer((prev) => {
+            return {...prev, [index]:value}
+        });
+    }
+
+       let calculatedScore = 0;
+        questionBank.forEach((question, index) => {
+            if(answer[index] === question.A) {
+                calculatedScore += 1;
+            }
+        });
+
   return (
-    <>
+    <QuestionContext.Provider value={{
+            questionBank,
+            answer,
+            qIndex,
+            setQindex,
+            handleAnswers,
+            calculatedScore
+        }}>
       <Question />
-    </>
+    </QuestionContext.Provider>
   )
 }
 
--- QuizApp/src/components/Question.jsx
+++ QuizApp/src/components/Question.jsx
@@ -1,41 +1,15 @@
-import { useState, createContext } from "react";
-import { values } from "./Values";
+import { createContext } from "react";
 import QuizPallet from "./QuizPallet";
 import Quiz from "./Quiz";
 
 export const QuestionContext = createContext();
 
 function Question(){
-
-    const questionBank = values[0].questions;
-    const [answer, setAnswer] = useState({});
-    const [qIndex, setQindex] = useState(0);
-
-    const handleAnswers = (index, value) =>{
-        setAnswer((prev) => {
-            return {...prev, [index]:value}
-        });
-    }
-
-       let calculatedScore = 0;
-        questionBank.forEach((question, index) => {
-            if(answer[index] === question.A) {
-                calculatedScore += 1;
-            }
-        });
-    
     return(
-        <QuestionContext.Provider value={{
-            questionBank,
-            answer,
-            qIndex,
-            setQindex,
-            handleAnswers,
-            calculatedScore
-        }}>
+        <>
             < QuizPallet />
             < Quiz />
-        </QuestionContext.Provider>
+        </>
     );
 }
 
